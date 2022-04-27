9473d3049ee90b439d1f3a05994092e9f2bcd466
student <student@mosilab.local>
2022-04-27 11:08:28 +0300

    knex

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,19 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { resolve } from "path";
-import sqlite3 from "sqlite3";
-
-// new sqlite3.Database('')
+import knex from 'knex'
 
 console.log(resolve("."));
 
-type Data = {
-  name: string;
-};
+
+(async () => {
+  const db = knex({
+    client: 'sqlite3',
+    connection: {
+      filename: './databases/database.db',
+    },
+  });
+  
+  await db.schema.createTableIfNotExists('users', (table) => {
+    table.increments('id', {
+      primaryKey: true,
+    });
+    table.string('username');
+    table.string('password');
+    table.string('email');
+  });
+  
+  await db("users").insert({ username: "Flafy", password: "123123", email: "CoolMail@gmail.com"});
+})()
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   res.status(200).json({ name: "John Doe" });
 }
